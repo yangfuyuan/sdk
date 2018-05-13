aa092634e3c99a66a8c2750af4f37afc3a0eb044
yangfuyuan <chushuifurong618@126.com>
2018-05-13 13:22:34 +0800

    update port input

--- samples/main.cpp
+++ samples/main.cpp
@@ -20,51 +20,38 @@ static void Stop(int signo)
 
 int main(int argc, char * argv[])
 {
-
-  bool showHelp  = argc>1 && !strcmp(argv[1],"--help");
 	printf(" YDLIDAR C++ TEST\n");
-    if (argc<4 || showHelp )
-	{
-      
-			printf("Usage: %s <serial_port> <baudrate> <intensities>\n\n",argv[0]);
-            printf("Example:%s /dev/ttyUSB0 115200 0\n\n",argv[0]);
-			if (!showHelp)
-			{				
-                return -1;
-			}
-            else
-                return 0;
-    }
-
-    const std::string port = string(argv[1]);
-    const int baud =  atoi(argv[2]);
-    const int intensities =  atoi(argv[3]);
+    std::string port;
+    std::string baudrate;
+    printf("Please enter the lidar port:");
+    std::cin>>port;
+    printf("Please enter the lidar baud rate:");
+    std::cin>>baudrate;
+    const int baud = atoi(baudrate.c_str());
+    const int intensities =  0;
 
     signal(SIGINT, Stop);
     signal(SIGTERM, Stop);
 
-  laser.setSerialPort(port);
-  laser.setSerialBaudrate(baud);
-  laser.setFixedResolution(false);
-  laser.setIntensities(intensities);
-
-  laser.initialize();
+    laser.setSerialPort(port);
+    laser.setSerialBaudrate(baud);
+    laser.setFixedResolution(false);
+    laser.setIntensities(intensities);
+    laser.initialize();
     while(!running){
 		bool hardError;
 		LaserScan scan;
 
 		if(laser.doProcessSimple(scan, hardError )){
-			fprintf(stderr,"Scan received: %u ranges\n",(unsigned int)scan.ranges.size());
-
+            fprintf(stdout,"Scan received: %u ranges\n",(unsigned int)scan.ranges.size());
+            fflush(stdout);
 		}
-    usleep(50*1000);
-
-		
+        usleep(50*1000);
 	}
-  laser.turnOff();
-  laser.disconnecting();
 
-  return 0;
+    laser.turnOff();
+    laser.disconnecting();
+    return 0;
 
 
 }
