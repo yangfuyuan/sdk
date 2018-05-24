24cb79537ceaec1b1755f260c64b626f010c5f14
yangfuyuan <chushuifurong618@126.com>
2018-05-24 10:59:24 +0800

    update document to V1.1.1

--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -6,10 +6,12 @@ var searchData=
   ['inputconfiguration',['InputConfiguration',['../class_s_l_1_1_input___lite_1_1_input_configuration.html',1,'SL::Input_Lite']]],
   ['inputmanager',['InputManager',['../class_s_l_1_1_input___lite_1_1_input_manager.html',1,'SL::Input_Lite']]],
   ['inter_5fbyte_5ftimeout',['inter_byte_timeout',['../structserial_1_1_timeout.html#ada15f2a0ae478cbb62ef79d1633b2b81',1,'serial::Timeout']]],
+  ['isautoconnting',['isAutoconnting',['../classydlidar_1_1_y_dlidar_driver.html#a4fcf172c55eabd5c6ca2d8c2bf14e5c6',1,'ydlidar::YDlidarDriver']]],
+  ['isautoreconnect',['isAutoReconnect',['../classydlidar_1_1_y_dlidar_driver.html#aa9fe773e62367a784c888d97c1886661',1,'ydlidar::YDlidarDriver']]],
   ['isconnected',['isconnected',['../classydlidar_1_1_y_dlidar_driver.html#a2553d28304faa4acab6bd63e7c6c2ee6',1,'ydlidar::YDlidarDriver::isconnected() const '],['../classydlidar_1_1_y_dlidar_driver.html#a4457578961d36ce22035d778c0fd7f06',1,'ydlidar::YDlidarDriver::isConnected()']]],
   ['isheartbeat',['isHeartbeat',['../classydlidar_1_1_y_dlidar_driver.html#a8a57494e849e7735fe8bf46d00b63077',1,'ydlidar::YDlidarDriver']]],
   ['isopen',['isOpen',['../classserial_1_1_serial.html#a48458f741d0fdcc5623b4daa079cbf78',1,'serial::Serial']]],
-  ['isscanning',['isScanning',['../classydlidar_1_1_y_dlidar_driver.html#a245115a8c8a4f98d988fbee06810d0a4',1,'ydlidar::YDlidarDriver::isScanning()'],['../classydlidar_1_1_y_dlidar_driver.html#a37cd2766dec3536848aa25c88d9d1ea9',1,'ydlidar::YDlidarDriver::isscanning() const ']]],
+  ['isscanning',['isscanning',['../classydlidar_1_1_y_dlidar_driver.html#a37cd2766dec3536848aa25c88d9d1ea9',1,'ydlidar::YDlidarDriver::isscanning() const '],['../classydlidar_1_1_y_dlidar_driver.html#a245115a8c8a4f98d988fbee06810d0a4',1,'ydlidar::YDlidarDriver::isScanning()']]],
   ['isvalid',['isvalid',['../structtouch__info.html#ad9c0bc30d781171b7f61a57de7df0ec9',1,'touch_info']]],
   ['input_5flite',['Input_lite',['../md__home_yang_tmp_sdk_input_lite_README.html',1,'']]]
 ];
