9881d30f33853991db7bc61b58f38111003e6d46
yangfuyuan <chushuifurong618@126.com>
2018-12-17 11:50:05 +0800

    fix r16 complie error

--- include/v8stdint.h
+++ include/v8stdint.h
@@ -127,7 +127,7 @@ set_signal_handler(int signal_value, signal_handler_t signal_handler)
     // NOLINTNEXTLINE(runtime/arrays)
     char error_string[error_length];
 #ifndef _WIN32
-#if (defined(_GNU_SOURCE) && !defined(ANDROID))
+#if (defined(_GNU_SOURCE) && !defined(ANDROID) &&(_POSIX_C_SOURCE >= 200112L))
     char * msg = strerror_r(errno, error_string, error_length);
     if (msg != error_string) {
       strncpy(error_string, msg, error_length);
